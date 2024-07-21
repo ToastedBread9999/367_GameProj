71b87cc3383ca1d21d0c3d42cb82240487482276
parthiban <you@example.com>
2024-07-21 13:27:54 +0800

    Main Menu

--- 367/Assets/Scripts/Menu.cs
+++ 367/Assets/Scripts/Menu.cs
@@ -12,6 +12,14 @@ public class Menu : MonoBehaviour
 
     public void QuitGame()
     {
+        Debug.Log("Quit");
+
+#if UNITY_EDITOR
+        // If running in the Unity Editor
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        // If running in a standalone build
         Application.Quit();
+#endif
     }
 }
