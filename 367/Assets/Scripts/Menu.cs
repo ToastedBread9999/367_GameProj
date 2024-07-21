a3ab92f697f0d02cbbbd7186ad32b47ac2441b01
JoeyGohShaoHong <1902125B@student.tp.edu.sg>
2024-07-21 22:07:38 +0800

    Added in new overlay for player
    Underwater look
    Updated the tutorial look

--- 367/Assets/Scripts/Menu.cs
+++ 367/Assets/Scripts/Menu.cs
@@ -8,7 +8,32 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene("Scene1"); // Replace with your actual scene name
     }
-   
+
+    public void LevelOneSelection()
+    {
+        SceneManager.LoadScene("Scene1"); 
+    }
+
+    public void LevelTwoSelection()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void LevelThreeSelection()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+
+    public void TutorialStage()
+    {
+        SceneManager.LoadScene("Tutorial Stage");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
 
     public void QuitGame()
     {
